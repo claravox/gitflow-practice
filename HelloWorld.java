78b870a247de654660223406af14d0b713ed9af2
claravox <claravox@users.noreply.github.com>
2021-04-12 14:23:26 -0700

    Add name to HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ import java.io.*;
 
 public class HelloWorld {
 	public static void main(String args[]) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World! This is Claire.");
 	}
 }
