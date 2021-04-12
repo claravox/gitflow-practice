33d5769de624b1b166deca57cc0f7f6a7b892030
kitty <yzhuan02@calpoly.edu>
2021-04-12 15:40:10 -0700

    Hi Kitty was here

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,13 @@ import java.io.*;
 public class HelloWorld {
 	public static void main(String args[]) {
 	  System.out.println("Hello World! This is Claire. I have been at Cal Poly for almost a year.");
+	  System.out.println("Hi Claire :) You're a very nice person Here's a flower for u");
+	  System.out.println("	,");
+	  System.out.println("	()    /)	  ");
+	  System.out.println("----.---'----(  )	  ");
+	  System.out.println("     \\        \\)");
+	  System.out.println("     ()                   VK	  ");
+	  System.out.println("      `	  ");
+
 	}
 }
